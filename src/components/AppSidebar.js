4056acf23ac9182bce9770a44dc96b204bf8b3ae
mamun2074 <mamun120520@gmail.com>
2025-12-14 21:16:09 +0600

    Remove header top unnecessory

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import {
-  CCloseButton,
-  CSidebar,
-  CSidebarBrand,
-  CSidebarFooter,
-  CSidebarHeader,
-  CSidebarToggler,
+    CCloseButton,
+    CSidebar,
+    CSidebarBrand,
+    CSidebarFooter,
+    CSidebarHeader,
+    CSidebarToggler,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 
@@ -20,48 +20,48 @@ import { sygnet } from 'src/assets/brand/sygnet'
 import navigation from '../_nav'
 
 const AppSidebar = () => {
-  const dispatch = useDispatch()
-  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+    const dispatch = useDispatch()
+    const unfoldable = useSelector((state) => state.sidebarUnfoldable)
+    const sidebarShow = useSelector((state) => state.sidebarShow)
 
-  return (
-    <CSidebar
-      className="border-end"
-      colorScheme="dark"
-      position="fixed"
-      unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
-      }}
-    >
-        
-      <CSidebarHeader className="border-bottom">
-        <CSidebarBrand to="/">
-          
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
-          
-        </CSidebarBrand>
-        <CCloseButton
-          className="d-lg-none"
-          dark
-          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
-        />
-      </CSidebarHeader>
+    return (
+        <CSidebar
+            className="border-end"
+            colorScheme="dark"
+            position="fixed"
+            unfoldable={unfoldable}
+            visible={sidebarShow}
+            onVisibleChange={(visible) => {
+                dispatch({ type: 'set', sidebarShow: visible })
+            }}
+        >
+
+            <CSidebarHeader className="border-bottom">
+                <CSidebarBrand to="/">
+
+                    <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
+                    <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+
+                </CSidebarBrand>
+                <CCloseButton
+                    className="d-lg-none"
+                    dark
+                    onClick={() => dispatch({ type: 'set', sidebarShow: false })}
+                />
+            </CSidebarHeader>
 
 
 
-      <AppSidebarNav items={navigation} />
+            <AppSidebarNav items={navigation} />
 
-      <CSidebarFooter className="border-top d-none d-lg-flex">
-        <CSidebarToggler
-          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
-        />
-      </CSidebarFooter>
+            <CSidebarFooter className="border-top d-none d-lg-flex">
+                <CSidebarToggler
+                    onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+                />
+            </CSidebarFooter>
 
-    </CSidebar>
-  )
+        </CSidebar>
+    )
 }
 
 export default React.memo(AppSidebar)
